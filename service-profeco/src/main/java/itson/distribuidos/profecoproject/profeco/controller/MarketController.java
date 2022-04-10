e62611ab27ce52ddb78189ba73d31a018c720e97
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2022-04-10 14:38:54 -0700

    Gateway added and more  - Gateway service added  - Mini change en path for markets in profeco microservice

--- service-profeco/src/main/java/itson/distribuidos/profecoproject/profeco/controller/MarketController.java
+++ service-profeco/src/main/java/itson/distribuidos/profecoproject/profeco/controller/MarketController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/markets")
+@RequestMapping(value = "/marketspr")
 public class MarketController {
     @Autowired
     private MarketService marketService;
