beedaf1db806d59237e8f6e482a839c0092e4dcd
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2022-04-10 12:48:06 -0700

    Added registry service and more  - Added registry service  - Modified all three microservices with eureka

--- service-consumer/src/main/java/itson/distribuidos/profecoproject/consumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/itson/distribuidos/profecoproject/consumer/ServiceConsumerApplication.java
@@ -2,8 +2,10 @@ package itson.distribuidos.profecoproject.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceConsumerApplication {
 
 	public static void main(String[] args) {
--- service-market/src/main/java/itson/distribuidos/profecoproject/market/ServiceMarketApplication.java
+++ service-market/src/main/java/itson/distribuidos/profecoproject/market/ServiceMarketApplication.java
@@ -2,8 +2,10 @@ package itson.distribuidos.profecoproject.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceMarketApplication {
 
 	public static void main(String[] args) {
